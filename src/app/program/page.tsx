8282296e283c9c627dba91118b489ad128cd7d7e
christianlasam <christianlasam1@gmail.com>
2026-02-09 21:48:26 -0800

    Created logic for basic lift selection

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -1,86 +1,101 @@
 "use client";
 
 import NavButtons from "@/components/ui/nav-buttons";
-import {Button} from "@/components/ui/button";  
-import { Select, SelectTrigger, SelectContent, SelectGroup, SelectItem, SelectValue } from "@/components/ui/select";
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectValue,
+} from "@/components/ui/select";
 import liftsData from "@/src/data/lifts.json";
 import { createContext, useContext, useState } from "react";
-const workoutContext = createContext<string>("");
-const liftContext = createContext<string>("");
-
+//const workoutContext = createContext<string>("");
+//const liftContext = createContext<string>("");
+//const variationContext = createContext<string>("");
 
 export default function Program() {
-    return (
-        <div className="flex flex-col min-h-screen">
-            <div className="flex justify-center items-center p-6">
-                <NavButtons active={1} />
-            </div>
-            <div className="flex justify-center items-center p-6">
-                <Button>Create Program</Button>
-            </div>
-            <div className="flex justify-center items-center p-6">
-                <Button>View Existing Programs</Button>
-            </div>
-            <CreateProgram />
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="flex justify-center items-center p-6">
+        <NavButtons active={1} />
+      </div>
+      <div className="flex justify-center items-center p-6">
+        <Button>Create Program</Button>
+      </div>
+      <div className="flex justify-center items-center p-6">
+        <Button>View Existing Programs</Button>
+      </div>
+      <CreateProgram />
+    </div>
+  );
 }
 
 function CreateProgram() {
-    const [type, setType] = useState("");
-    return (
-        <workoutContext.Provider value={type}>
-            <div className="flex flex-col items-center gap-4">
-                <Select onValueChange={setType} value={type}>
-                  <SelectTrigger className="w-full max-w-48">
-                <SelectValue placeholder="Select a Workout" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectItem value="Main">Main</SelectItem>
-                      <SelectItem value="Accessory">Accessory</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-                <Main />
-            </div>
-        </workoutContext.Provider>
-    );
-}
+  const [type, setType] = useState("");
+  const [workout, setWorkout] = useState("");
+  const [variation, setVariation] = useState("");
 
-function Main() {
-    const mainLifts = Object.keys(liftsData.Main);
-    const accessoryLifts = Object.keys(liftsData.Accessory);
-    const type = useContext(workoutContext);
-    const [workout, setWorkout] = useState("");
-    return (
-        <liftContext.Provider value={workout}>
-            <Select>
-              <SelectTrigger className="w-full max-w-48">
-                <SelectValue placeholder="Select a Workout" />
-              </SelectTrigger>
-              <SelectContent>
-                {type === "Main" ? (
-                  <SelectGroup>
-                    {mainLifts.map((l) => (
-                      <SelectItem key={l} value={l}>
-                        {l}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                ) : (
-                  <SelectGroup>
-                    {accessoryLifts.map((l) => (
-                      <SelectItem key={l} value={l}>
-                        {l}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                )}
-                
-              </SelectContent>
-            </Select>
-        </liftContext.Provider>
-    );
+  let lifts: string[] = [];
+  if (type === "Main") {
+    // @ts-ignore
+    lifts = Object.keys(liftsData.Main) || [];
+  } else if (type === "Accessory") {
+    // @ts-ignore
+    lifts = Object.keys(liftsData.Accessory) || [];
+  }
+
+  let variations: string[] = [];
+  if (type === "Main" && workout) {
+    // @ts-ignore
+    variations = liftsData.Main[workout] || [];
+  } else if (type === "Accessory" && workout) {
+    // @ts-ignore
+    variations = liftsData.Accessory[workout] || [];
+  }
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <Select onValueChange={setType} value={type}>
+        <SelectTrigger className="w-full max-w-48">
+          <SelectValue placeholder="Select a Workout" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectItem value="Main">Main</SelectItem>
+            <SelectItem value="Accessory">Accessory</SelectItem>
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Select onValueChange={setWorkout} value={workout}>
+        <SelectTrigger className="w-full max-w-48">
+          <SelectValue placeholder="Select a Workout" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {lifts.map((l) => (
+              <SelectItem key={l} value={l}>
+                {l}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+      <Select onValueChange={setVariation} value={variation}>
+        <SelectTrigger className="w-full max-w-48">
+          <SelectValue placeholder="Select a Workout" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            {variations.map((v) => (
+              <SelectItem key={v} value={v}>
+                {v}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
+  );
 }
-    
