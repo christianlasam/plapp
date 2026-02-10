11a511d08784a3afabe9b5c23bd2aad288c5148c
christianlasam <christianlasam1@gmail.com>
2026-02-09 16:52:31 -0800

    Set up basic context provider for app

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import NavButtons from "@/components/ui/nav-buttons";
-import {Button} from "@/components/ui/button";
+import {Button} from "@/components/ui/button";  
+import { Select, SelectTrigger, SelectContent, SelectGroup, SelectItem, SelectValue } from "@/components/ui/select";
+import liftsData from "@/src/data/lifts.json";
+import { createContext, useContext, useState } from "react";
+const workoutContext = createContext<string>("");
+const liftContext = createContext<string>("");
+
+
 export default function Program() {
     return (
         <div className="flex flex-col min-h-screen">
@@ -9,7 +18,69 @@ export default function Program() {
             <div className="flex justify-center items-center p-6">
                 <Button>Create Program</Button>
             </div>
+            <div className="flex justify-center items-center p-6">
+                <Button>View Existing Programs</Button>
+            </div>
+            <CreateProgram />
         </div>
-        
     );
 }
+
+function CreateProgram() {
+    const [type, setType] = useState("");
+    return (
+        <workoutContext.Provider value={type}>
+            <div className="flex flex-col items-center gap-4">
+                <Select onValueChange={setType} value={type}>
+                  <SelectTrigger className="w-full max-w-48">
+                <SelectValue placeholder="Select a Workout" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="Main">Main</SelectItem>
+                      <SelectItem value="Accessory">Accessory</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+                <Main />
+            </div>
+        </workoutContext.Provider>
+    );
+}
+
+function Main() {
+    const mainLifts = Object.keys(liftsData.Main);
+    const accessoryLifts = Object.keys(liftsData.Accessory);
+    const type = useContext(workoutContext);
+    const [workout, setWorkout] = useState("");
+    return (
+        <liftContext.Provider value={workout}>
+            <Select>
+              <SelectTrigger className="w-full max-w-48">
+                <SelectValue placeholder="Select a Workout" />
+              </SelectTrigger>
+              <SelectContent>
+                {type === "Main" ? (
+                  <SelectGroup>
+                    {mainLifts.map((l) => (
+                      <SelectItem key={l} value={l}>
+                        {l}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                ) : (
+                  <SelectGroup>
+                    {accessoryLifts.map((l) => (
+                      <SelectItem key={l} value={l}>
+                        {l}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                )}
+                
+              </SelectContent>
+            </Select>
+        </liftContext.Provider>
+    );
+}
+    
